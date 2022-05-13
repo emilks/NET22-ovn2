5cc389e37cd19350fb89a292c73485b12e9e5dc0
emilks <emil.keski@gmail.com>
2022-05-13 14:26:33 +0200

    Add project files.

---
+++ Övning 2/Program.cs
@@ -0,0 +1,20 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello, World!");
+
+bool active = true;
+while (active)
+{
+    Console.WriteLine("Välkommna till ");
+    int choice = Convert.ToInt32(Console.ReadLine());
+    switch(choice)
+    {
+        case 0:
+            Console.WriteLine("Hej då");
+            active = false;
+            break;
+        default:
+            Console.WriteLine("Felaktig input");
+            break;
+
+    }
+}
